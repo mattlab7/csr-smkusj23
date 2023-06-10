125c6f54afc9c6f4253ba877f64d3fd04ce95798
Matt <43501106+mattlab7@users.noreply.github.com>
2023-06-10 20:06:02 +0800

    feat/real home page

--- index.php
+++ index.php
@@ -2,11 +2,39 @@
 <html lang="en">
 <head>
     <title>Document</title>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
+
+    <!-- Bootstrap CSS  -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <!-- Bootstrap JS and its prerequisites -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.7/dist/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 </head>
-<body>
-    <h1>
-        <?php echo "Hello World"; ?>
-    </h1>
+<body class="bg-dark text-light">
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark px-4 text-light">
+        <a class="navbar-brand" href="#">SMK USJ23</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Login</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Register</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+
+    <div class="container mt-4 text-center">
+        <img src="https://upload.wikimedia.org/wikipedia/ms/7/79/Sekolah_Menengah_Kebangsaan_USJ_23.png" alt="Logo SMK USJ 23" class="py-3">
+        <h1>Welcome to <strong>SMK USJ23</strong></h1>
+        <p><u>Sekolah Menengah Kebangsaan USJ 23</u> or its short name <em>SMK USJ 23</em>, is a secondary school that is located at Jalan Harmoni.</p>
+        <p>In 2009, the school logged an amount of 1391 students and 87 teachers.</p>
+        <cite>Sekolah Menengah Kebangsaan USJ 23. (2017, January 19). Wikipedia. Retrieved from <a href="https://ms.wikipedia.org/wiki/Sekolah_Menengah_Kebangsaan_USJ_23">ms.wikipedia.org</a></cite>
+    </div>
+
 </body>
 </html>
